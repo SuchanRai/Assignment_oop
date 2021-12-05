4ee4e5e7438e4c0ebac211c881c90129c07fb98a
lingk <lingkhimraee@gmail.com>
2021-12-06 01:03:22 +0545

    Asscending

---
+++ src/Asscending.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Asscending {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the student's number: ");
+        int n = sc.nextInt();
+        String[] str = new String[n];
+        System.out.println("Enter strings: ");
+        for (int i = 0; i < n; i++) {
+            str[i] = new String(sc.next());
+        }
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (str[i].compareTo(str[j]) > 0) {
+                    String tempt = str[i];
+                    str[i] = str[j];
+                    str[j] = tempt;
+
+                }
+            }
+        }
+        System.out.println("sorted list of strings is; ");
+        for (int i = 0; i < n; i++){
+            System.out.println(str[i]);
+        }
+
+    }
+
+}
+
--- src/Palindrome.java
+++ src/Palindrome.java
@@ -2,11 +2,10 @@ import java.util.Scanner;
 
 public class Palindrome {
     public static void main(String[] args) {
-        String word;
-        String reverse="";
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter a number or Word");
-        word = sc.nextLine();
+        String word= sc.nextLine();
+        String reverse="";
         int length = word.length();
         for (int i = length-1; i>=0;i = i-1)
             reverse = reverse + word.charAt(i);
