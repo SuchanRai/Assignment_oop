4302f19bb672669220fe47080fadc3544a20166e
lingk <lingkhimraee@gmail.com>
2021-12-05 11:06:55 +0545

    Initial commit

---
+++ src/Prime_number.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Prime_number {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int number = sc.nextInt();
+        int count;
+
+
+        for (int i = 1; i <= number; i++) {
+            count = 0;
+            for (int j = 2; j <= i / 2; j++) {
+                if (i % j == 0) {
+                    count++;
+                    break;
+                }
+            }
+            if (count == 0) {
+                System.out.println(i);
+            }
+        }
+
+    }
+}
